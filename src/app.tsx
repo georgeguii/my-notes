f08380bdfc31fde3808a8b676b900bffec452e70
George Guilherme <george.gui2000@gmail.com>
2024-02-11 12:02:46 -0300

    :recycle: refactor: configurada base da tipografia no tailwind

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,6 @@
 export function App() {
   return (
-    <h1 className="text-gray-200 text-3xl font-bold underline">
+    <h1 className="text-3xl font-bold underline">
       Hello, World!
     </h1>
   )
