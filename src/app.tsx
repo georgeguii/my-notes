20772863d84d887d69b186ada0ff7af7f7c554c9
George Guilherme <george.gui2000@gmail.com>
2024-02-11 14:35:57 -0300

    :recycle: refactor: componetizando elementos da tela inicial

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,6 @@
 import logo from './assets/logo-nlw-expert.svg'
+import { NewNoteCard } from './components/new-note-card'
+import { NoteCard } from './components/note-card'
 
 export function App() {
   return (
@@ -16,37 +18,10 @@ export function App() {
       <div className='h-px bg-slate-700' />
 
       <div className='grid grid-cols-3 auto-rows-[250px] gap-6'>
-        <div className='rounded-md bg-slate-700 p-5 space-y-3'>
-          <span className='text-sm font-medium text-slate-200'>
-            Adicionar nota
-          </span>
-          <p className='text-sm leading-6 text-slate-400'>
-            Grave uma nota em áudio que será convertida para texto automaticamente.
-          </p>
-        </div>
-
-        <div className='rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative'>
-          <span className='text-sm font-medium text-slate-300'>
-            há 2 dias
-          </span>
-          <p className='text-sm leading-6 text-slate-400 '>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia numquam, enim eaque ducimus consequatur cum reiciendis dolores deserunt fugit vel provident dolore. Vel inventore qui nihil ipsum provident, totam eaque.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia numquam, enim eaque ducimus consequatur cum reiciendis dolores deserunt fugit vel provident dolore. Vel inventore qui nihil ipsum provident, totam eaque.
-          </p>
-
-          <div className='absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none' />
-        </div>
-
-        <div className='rounded-md bg-slate-800 p-5 space-y-3  overflow-hidden relative'>
-          <span className='text-sm font-medium text-slate-300'>
-            há 4 dias
-          </span>
-          <p className='text-sm leading-6 text-slate-400'>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi, saepe? Temporibus, asperiores. Eum aliquam ipsum blanditiis quis nisi, mollitia inventore omnis corporis molestias repellat sit laborum adipisci rerum dolore quos?
-          </p>
-
-          <div className='absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none' />
-        </div>
+        <NewNoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
       </div>
     </div>
   )
